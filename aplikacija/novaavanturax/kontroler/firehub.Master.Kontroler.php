f91fb45d91034bc99d466c62933747fa154a74de
Danijel Galić <56673130+dieselxxx@users.noreply.github.com>
2024-06-18 12:46:23 +0200

    initial

---
+++ aplikacija/novaavanturax/kontroler/firehub.Master.Kontroler.php
@@ -0,0 +1,27 @@
+<?php declare(strict_types = 1);
+
+/**
+ * Master
+ * @since 0.1.2.pre-alpha.M1
+ *
+ * @author Danijel Galić
+ * @copyright 2022 Kapriol Web Trgovina
+ * @license GNU General Public License version 3 - [https://opensource.org/licenses/GPL-3.0](https://opensource.org/licenses/GPL-3.0)
+ *
+ * @version 1.0
+ * @package Aplikacija\Kontroler
+ */
+
+namespace FireHub\Aplikacija\NovaAvantura\Kontroler;
+
+use FireHub\Jezgra\Kontroler\Kontroler;
+
+/**
+ * ### Master
+ * @since 0.1.2.pre-alpha.M1
+ *
+ * @package Aplikacija\Kontroler
+ */
+abstract class Master_Kontroler extends Kontroler {
+
+}
---
+++ aplikacija/novaavanturax/kontroler/firehub.Naslovna.Kontroler.php
@@ -0,0 +1,40 @@
+<?php declare(strict_types = 1);
+
+/**
+ * Naslovna
+ * @since 0.1.0.pre-alpha.M1
+ *
+ * @author Danijel Galić
+ * @copyright 2022 Kapriol Web Trgovina
+ * @license GNU General Public License version 3 - [https://opensource.org/licenses/GPL-3.0](https://opensource.org/licenses/GPL-3.0)
+ *
+ * @version 1.0
+ * @package Aplikacija\Kontroler
+ */
+
+namespace FireHub\Aplikacija\NovaAvantura\Kontroler;
+
+use FireHub\Jezgra\Komponente\BazaPodataka\BazaPodataka;
+use FireHub\Jezgra\Sadrzaj\Sadrzaj;
+
+/**
+ * ### Naslovna
+ * @since 0.1.0.pre-alpha.M1
+ *
+ * @package Aplikacija\Kontroler
+ */
+final class Naslovna_Kontroler extends Master_Kontroler {
+
+    /**
+     * ## index
+     * @since 0.1.0.pre-alpha.M1
+     *
+     * @return Sadrzaj Sadržaj stranice.
+     */
+    public function index (BazaPodataka $bazaPodataka = null):Sadrzaj {
+
+        return sadrzaj()->datoteka('naslovna.html')->podatci([]);
+
+    }
+
+}
