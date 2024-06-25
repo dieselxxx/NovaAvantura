4e972d1854f3d8df57db9d492ecc290e4d32f138
Danijel Galić <56673130+dieselxxx@users.noreply.github.com>
2024-06-25 15:07:53 +0200

    initial

--- web/novaavantura/resursi/teme/novaavantura/js/novaavantura.js
+++ web/novaavantura/resursi/teme/novaavantura/js/novaavantura.js
@@ -6,4 +6,12 @@ $(document).ready(function () {
         $el.toggleClass('skrolanje', $(this).scrollTop() > 200);
     });
 
+    // tražilica
+    $('header a.trazi').click(function(event) {
+        $("#trazi").animate({
+            height: 'toggle'
+        });
+        $("header > .pozadina").toggle();
+    });
+
 });
