c3902273adbe93d1496d779bee4da1da8228648d
Danijel Galić <56673130+dieselxxx@users.noreply.github.com>
2024-06-26 12:39:17 +0200

    initial

--- web/novaavantura/resursi/teme/novaavantura/js/novaavantura.js
+++ web/novaavantura/resursi/teme/novaavantura/js/novaavantura.js
@@ -1,5 +1,6 @@
 $(document).ready(function () {
 
+
     // skrolanje zaglavlja
     $(window).scroll(function(e){
         $el = $('header');
@@ -13,5 +14,32 @@ $(document).ready(function () {
         });
         $("header > .pozadina").toggle();
     });
+
+    // tabovi
+    var acc = document.getElementsByClassName("accordion");
+    var acc2 = document.getElementsByClassName("accordion2");
+    var i;
+    for (i = 0; i < acc.length; i++) {
+        acc[i].addEventListener("click", function() {
+            this.classList.toggle("active");
+            var panel = this.nextElementSibling;
+            if (panel.style.maxHeight) {
+                panel.style.maxHeight = null;
+            } else {
+                panel.style.maxHeight = panel.scrollHeight + "px";
+            }
+        });
+    }
+    for (i = 0; i < acc2.length; i++) {
+        acc2[i].addEventListener("click", function() {
+            this.classList.toggle("active");
+            var panel = this.nextElementSibling;
+            if (panel.style.maxHeight) {
+                panel.style.maxHeight = null;
+            } else {
+                panel.style.maxHeight = panel.scrollHeight + "px";
+            }
+        });
+    }
 
 });
