ec8818cf553780c8db069a2fb5a0d3319e95f855
Danijel Galić <56673130+dieselxxx@users.noreply.github.com>
2024-07-02 13:14:33 +0200

    initial

--- web/novaavantura/resursi/teme/novaavantura/js/novaavantura.js
+++ web/novaavantura/resursi/teme/novaavantura/js/novaavantura.js
@@ -1,46 +1,7 @@
 $(document).ready(function () {
 
 
-    // skrolanje zaglavlja
-    $(window).scroll(function(e){
-        $el = $('header');
-        $el.toggleClass('skrolanje', $(this).scrollTop() > 200);
-    });
 
-    // tražilica
-    $('header a.trazi').click(function(event) {
-        $("#trazi").animate({
-            height: 'toggle'
-        });
-        $("header > .pozadina").toggle();
-    });
-
-    // tabovi
-    var acc = document.getElementsByClassName("accordion");
-    var acc2 = document.getElementsByClassName("accordion2");
-    var i;
-    for (i = 0; i < acc.length; i++) {
-        acc[i].addEventListener("click", function() {
-            this.classList.toggle("active");
-            var panel = this.nextElementSibling;
-            if (panel.style.maxHeight) {
-                panel.style.maxHeight = null;
-            } else {
-                panel.style.maxHeight = panel.scrollHeight + "px";
-            }
-        });
-    }
-    for (i = 0; i < acc2.length; i++) {
-        acc2[i].addEventListener("click", function() {
-            this.classList.toggle("active");
-            var panel = this.nextElementSibling;
-            if (panel.style.maxHeight) {
-                panel.style.maxHeight = null;
-            } else {
-                panel.style.maxHeight = panel.scrollHeight + "px";
-            }
-        });
-    }
 
     // naslovna swiper
     var swiper = new Swiper(".rotator", {
