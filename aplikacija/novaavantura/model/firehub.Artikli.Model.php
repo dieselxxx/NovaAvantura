81b5190d0c60148ac8226a39bd66b295b4ee33ee
Danijel Galić <56673130+dieselxxx@users.noreply.github.com>
2024-07-10 09:46:11 +0200

    initial

--- aplikacija/novaavantura/model/firehub.Artikli.Model.php
+++ aplikacija/novaavantura/model/firehub.Artikli.Model.php
@@ -72,12 +72,24 @@ final class Artikli_Model extends Master_Model {
             default => "KategorijaID = '$kategorija'"
         };
 
+        $poredaj = match ($poredaj) {
+            'naziv' => 'Naziv',
+            'starost' => 'DostupnoOd',
+            default => 'CijenaIliAkcija'
+        };
+
+        $poredaj_redoslijed = match ($poredaj_redoslijed) {
+            'desc' => 'desc',
+            default => 'asc'
+        };
+
         $rezultat = $this->bazaPodataka->tabela('artikliview')
             ->sirovi("
                     SELECT
                        artikliview.ID, Naziv, Link, Opis,
                        ".Domena::sqlCijena()." AS Cijena,
                        ".Domena::sqlCijenaAkcija()." AS CijenaAkcija,
+                       IF(".Domena::sqlCijenaAkcija()." > 0, ".Domena::sqlCijenaAkcija().", ".Domena::sqlCijena().") AS CijenaIliAkcija,
                        GROUP_CONCAT(DISTINCT artiklikarakteristike.Velicina) AS Velicine,
                        brandovi.Brand,
                        (SELECT Slika FROM slikeartikal WHERE slikeartikal.ClanakID = artikliview.ID ORDER BY slikeartikal.Zadana DESC LIMIT 1) AS Slika,
